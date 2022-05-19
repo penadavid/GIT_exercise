a97e20a13ea2fc724f1bfd4f8a77e2da11178d76
penadavid <davidpena1598@gmail.com>
2022-05-20 00:27:54 +0200

    Hello world

--- Vezbe1/Zad1/main.c
+++ Vezbe1/Zad1/main.c
@@ -1,1 +1,18 @@
-sdasdas
+# include <avr /io.h>
+# include <util / delay .h>
+# include <stdint .h>
+int16_t main ()
+{
+int16_t high_time = 500;
+int16_t low_time = 500;
+DDRB |= 1 << 5; // PB5 je izlaz
+while (1)
+{
+PORTB |= 1 << 5; // LED ON
+_delay_ms ( high_time ); // Pauza 1s
+PORTB &= ~(1 << 5); // LED OFF
+_delay_ms ( low_time ); // Pauza 1s
+}
+return 0;
+}
+Listing 1
